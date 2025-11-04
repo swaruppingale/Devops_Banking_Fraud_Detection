4ae81bc74884e9d3a2be9646b977b5df082b20e9
Swarup Pingale <swaruppingale04@gmail.com>
2025-11-05 00:40:50 +0530

    Updated fraud model with synthetic fraud learning

--- train_model.py
+++ train_model.py
@@ -1,18 +1,22 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-print("🔄 Loading dataset...")
-df = pd.read_csv("transactions.csv")
+def predict_on_new(data):
+    model = joblib.load("fraud_model.joblib")
+    preds = model.predict(data)
 
-X = df[['amount','frequency','location_score','hour']]
-y = df['fraud_flag']
+    # Add ML prediction
+    data["ml_prediction"] = preds
 
-print("🤖 Training model...")
-model = RandomForestClassifier(n_estimators=200, random_state=7)
-model.fit(X, y)
+    # RULE-BASED FRAUD LOGIC (Hybrid System)
+    data["rule_flag"] = data.apply(lambda row: 1 if (row["amount"] > 20000 and row["location_score"] < 0.3) else 0, axis=1)
 
-print("💾 Saving new model...")
-joblib.dump(model, "fraud_model.joblib")
+    # Final fraud flag = ML or Rule
+    data["fraud_prediction"] = data.apply(lambda row: 1 if (row["ml_prediction"] == 1 or row["rule_flag"] == 1) else 0, axis=1)
 
-print("✅ Model retrained successfully! Now you can run fraud_detector.py")
+    data.to_csv("predictions.csv", index=False)
+    print("✅ Fraud detection completed. Check predictions.csv file.")
+
+if __name__ == "__main__":
+    df = pd.read_csv("new_transactions.csv")
+    predict_on_new(df)
